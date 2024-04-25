c9a513b803d936a914321047e9e5c34791ef99b4
zhaojunfeng <zhaojunfeng@hellogroup.com>
2024-04-25 18:36:04 +0800

    update bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-mod avutil;
+pub mod avutil;
 
 pub mod binding;
-#[allow(
-    non_snake_case,
-    non_camel_case_types,
-    non_upper_case_globals,
-    improper_ctypes,
-    clippy::all
-)]
-pub mod ffi {
-    pub use crate::binding::*;
-    pub use crate::avutil::{_avutil::*, common::*, error::*, pixfmt::*, rational::*};
-    // include!(concat!(env!("OUT_DIR"), "/binding.rs"));
-
-}
+// #[allow(
+//     non_snake_case,
+//     non_camel_case_types,
+//     non_upper_case_globals,
+//     improper_ctypes,
+//     clippy::all
+// )]
+// pub mod ffi {
+//     pub use crate::binding::*;
+//     pub use crate::avutil::{_avutil::*, common::*, error::*, pixfmt::*, rational::*};
+//     // include!(concat!(env!("OUT_DIR"), "/binding.rs"));
+//
+// }
